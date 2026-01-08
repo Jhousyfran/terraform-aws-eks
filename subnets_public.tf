aba277d030b93aa7e9a8a5a349402cfa2ba69b0f
Jhousyfran Costa <jhousyfrancosta@gmail.com>
2026-01-08 00:10:05 -0300

    Adiciona configuração inicial do Terraform para o EKS, incluindo VPC, sub-redes, variáveis e provedores.

---
+++ subnets_public.tf
@@ -0,0 +1,29 @@
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+  availability_zone       = "${data.aws_region.current.region}a"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "${var.prefix}-public-subnet-1a"
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
+}
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+  availability_zone       = "${data.aws_region.current.region}b"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "${var.prefix}-public-subnet-1b"
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "prefix" {
+  description = "Prefix para nomear os recursos"
+  type        = string
+  default     = "terraform"
+}
+
+variable "environment" {
+  description = "Ambiente onde os recursos serão criados"
+  type        = string
+  default     = "dev"
+}
+
+variable "project" {
+  description = "Nome do projeto"
+  type        = string
+  default     = "eks-setup"
+}
+
+variable "region" {
+  description = "Região AWS onde os recursos serão criados"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Nome do cluster EKS"
+  type        = string
+  default     = "eks-cluster"
+}
+
+variable "cidr_block" {
+  description = "Bloco CIDR para a VPC"
+  type        = string
+}
+
+
