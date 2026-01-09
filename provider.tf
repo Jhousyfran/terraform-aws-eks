6aaf5c644e6e1b7f1e81a3ca63e947eebcdce378
Jhousyfran Costa <jhousyfrancosta@gmail.com>
2026-01-09 17:53:34 -0300

    Atualiza configuração dos provedores AWS, Kubernetes e Helm, e renomeia variável de região para aws_region

--- provider.tf
+++ provider.tf
@@ -4,10 +4,40 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.27.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.0.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
+  region = var.aws_region
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.eks_certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.eks_cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks_cluster.eks_cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.eks_certificate_authority)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.eks_cluster_name]
+      command     = "aws"
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "project" {
   default     = "eks-setup"
 }
 
-variable "region" {
+variable "aws_region" {
   description = "Região AWS onde os recursos serão criados"
   type        = string
   default     = "us-east-1"
