6b8214d0888fcb8960c74b98fb2a46453dbc1326
Jhousyfran Costa <jhousyfrancosta@gmail.com>
2026-01-09 16:14:42 -0300

    Adiciona configuração do controlador de balanceador de carga AWS, incluindo funções IAM, políticas e variáveis necessárias

---
+++ modules/aws-loadbalacer-controller/variables.tf
@@ -0,0 +1,20 @@
+variable "prefix" {
+  description = "Prefix para nomear os recursos"
+  type        = string
+}
+
+variable "project" {
+  description = "Nome do projeto"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags para aplicar aos recursos"
+  type        = map(string)
+  default     = {}
+}
+
+variable "oidc" {
+  description = "HTTPS URL  OIDC do cluster EKS"
+  type        = string
+}
