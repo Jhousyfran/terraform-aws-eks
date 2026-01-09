dc0395bc2331b38c4e626635ce5edae74e801868
Jhousyfran Costa <jhousyfrancosta@gmail.com>
2026-01-09 17:54:29 -0300

    Adiciona configuração dos provedores Helm e Kubernetes no arquivo .terraform.lock.hcl e ajusta módulos no arquivo modules.tf

--- modules.tf
+++ modules.tf
@@ -16,3 +16,24 @@ module "eks_cluster" {
   public_subnet_1b_id = module.network.eks_subnet_public_1b_id
 
 }
+
+module "managed_node_group" {
+  source  = "./modules/managed-node-group"
+  prefix  = var.prefix
+  project = var.project
+  tags    = local.tags
+
+  eks_cluster_name     = module.eks_cluster.eks_cluster_name
+  private_subnet_1a_id = module.network.eks_subnet_private_1a_id
+  private_subnet_1b_id = module.network.eks_subnet_private_1b_id
+}
+
+module "eks_loadbalancer_controller" {
+  source           = "./modules/aws-loadbalacer-controller"
+  prefix           = var.prefix
+  project          = var.project
+  tags             = local.tags
+  oidc             = module.eks_cluster.oidc
+  eks_cluster_name = module.eks_cluster.eks_cluster_name
+
+}
